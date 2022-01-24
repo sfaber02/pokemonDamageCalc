53d2f49a742101b7e7a2c1c49a8ca7918797215a
Shawn Faber <sfaber02@gmail.com>
2022-01-23 21:48:45 -0500

    final solved version

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
+const calculateDamage = (yourType, opponentType, attack, defense) => {
+    const damageTable = {
+        "fire": {
+            "fire": 0.5,
+            "grass": 2,
+            "water": 0.5,
+            "electric": 1 
+        },
+        "grass": {
+            "fire": 0.5,
+            "grass": 0.5,
+            "water": 2,
+            "electric": 1 
+        },
+        "water": {
+            "fire": 2,
+            "grass": 0.5,
+            "water": 0.5,
+            "electric": 0.5
+        },
+        "electric": {
+            "fire": 1,
+            "grass": 1,
+            "water": 2,
+            "electric": 0.5 
+        }
+    }
+    return 50 * (attack / defense) * damageTable[yourType][opponentType];
+}
 
-
+console.log(calculateDamage("fire", "water", 100, 100));
 
 /*
 
